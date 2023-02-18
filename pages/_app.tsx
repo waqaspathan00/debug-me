1d3e4a8b88940829b94de749ede7b06b69dd1587
waqasabbasi <waqaspathan00@gmail.com>
2023-02-18 10:55:13 -0600

    add icon and toaster

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/global.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import React from "react";
+import {Toaster} from "react-hot-toast";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// @ts-ignore
+function MyApp({Component, pageProps}) {
+    return (
+        <>
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+            />
+            <title>DebugMe - AI Powered Code Debugger</title>
+            <Toaster/>
+            <Component {...pageProps} />
+        </>
+    );
 }
+
+export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head>
+                <link rel="shortcut icon" type="image/jpg" href="/icon.png" />
+                <link rel="icon" type="image/x-icon" href="/icon.png" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
