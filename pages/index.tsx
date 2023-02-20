1ba099c88518776f72edd218bb4dd097c70c358a
waqasabbasi <waqaspathan00@gmail.com>
2023-02-20 02:11:22 -0500

    restyled inputs and added language selection for syntax highlighting

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,11 +11,13 @@ export default function Home() {
     const [problem, setProblem] = useState("")
     const [code, setCode] = useState("")
     const [resultCode, setResultCode] = useState("")
+    const [language, setLanguage] = useState("jsx");
 
     function handleAnswerSubmit() {
         const toastId = toast.loading("Processing...");
+        const prompt = `Problem: ${problem}\n${code ? `Code:\n${code}\n` : ""}Suggested solution: `;
         openai.createCompletion({
-            prompt: `Problem: ${problem}\nCode:\n${code}\nSuggested solution: `,
+            prompt: prompt,
             model: "text-davinci-003",
             max_tokens: 1024,
             temperature: 0.2
@@ -23,7 +25,7 @@ export default function Home() {
             //@ts-ignore
             const responseText: any = res.data.choices[0].text;
             console.log(responseText);
-            setResultCode(responseText);
+            setResultCode(responseText.trim());
         }).catch((err) => {
             console.log(err)
             toast.error("Something went wrong");
@@ -35,24 +37,26 @@ export default function Home() {
 
     return (
         <div className={"flex flex-col justify-start items-center h-screen"}>
-            <div className={"flex flex-col w-1/2 [&>*]:my-1 mt-10"}>
-                <input className={"w-full p-2 rounded-lg border-2"} placeholder={"Enter your problem"}
+            <div className={"flex flex-col items-center sm:w-1/2 w-full [&>*]:my-1 mt-10"}>
+                {/*input box that asks user to enter programming language*/}
+                <input onChange={(event: any) => {setLanguage(event.target.value)}} className={"w-11/12 p-2 rounded-lg border-2"} placeholder={"Enter your language"}/>
+                <textarea className={"w-11/12 p-2 rounded-lg border-2"} placeholder={"Enter your problem"}
                        onChange={(event) => {
                            setProblem(event.target.value)
                        }}/>
-                <textarea className={"w-full p-2 rounded-lg border-2"} placeholder={"Enter your code"}
+                <textarea className={"w-11/12 h-60 p-2 rounded-lg border-2"} placeholder={"Enter your code"}
                           onChange={(event) => {
                               setCode(event.target.value)
                           }}/>
                 <button
-                    className={"bg-green-600 hover:bg-green-700 transition-all text-white p-2 rounded-lg cursor-pointer"}
+                    className={"w-1/2 p-2 rounded-lg transition-all text-white duration-300 bg-gradient-to-r from-green-300 to-green-700 via-green-600 bg-size-200 bg-pos-0 hover:bg-pos-100"}
                     onClick={handleAnswerSubmit}>Submit
                 </button>
 
             </div>
 
             <div className={"w-full h-fit"}>
-                <Prism language="jsx" style={atomOneDark}>
+                <Prism language={language.toLowerCase()} style={atomOneDark}>
                     {resultCode}
                 </Prism>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,15 @@ module.exports = {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            backgroundSize: {
+                'size-200': '200% 200%',
+            },
+            backgroundPosition: {
+                'pos-0': '0% 0%',
+                'pos-100': '100% 100%',
+            },
+        },
     },
     plugins: [],
 }
